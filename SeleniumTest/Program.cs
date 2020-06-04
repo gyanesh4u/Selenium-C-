0cea1ee8189b5719e505a4ceda55800b8648739c
gyanesh <kamal.gyanesh8@gmail.com>
2020-06-04 23:32:30 +0530

    first selenium code with c#

---
+++ SeleniumTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace SeleniumTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://www.google.com");
+            driver.Manage().Window.Maximize();
+            driver.FindElement(By.Name("q")).SendKeys("selenium");
+            driver.FindElement(By.Name("btnK")).Click();
+            driver.Quit();
+        }
+    }
+}
