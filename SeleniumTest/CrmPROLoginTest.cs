b73b8ebbebd3626f8e9c65d041dd902930b32cd9
gyanesh <kamal.gyanesh8@gmail.com>
2020-06-05 01:43:00 +0530

    CRMPro login test

---
+++ SeleniumTest/CrmPROLoginTest.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeleniumTest
+{
+    class CrmPROLoginTest
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://www.crmpro.com/");
+            driver.FindElement(By.Name("username")).SendKeys("alok4u");
+            driver.FindElement(By.Name("password")).SendKeys("iamalok");
+            driver.FindElement(By.XPath("//input[@value='Login']")).Click();
+            Console.WriteLine(driver.Title);
+            driver.Quit();
+        }
+    }
+}
