42d0219fd2f8e7059510d1e1c2eff95843a95967
gyanesh <kamal.gyanesh8@gmail.com>
2020-06-05 01:06:07 +0530

    title test

---
+++ SeleniumTest/GoogleTitle.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace SeleniumTest
+{
+    class GoogleTitle
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://www.google.com");
+            driver.Manage().Window.Maximize();
+            string title = driver.Title;
+            Console.WriteLine("google title is " + title);
+            Console.WriteLine(driver.Url);
+            Console.WriteLine(driver.PageSource);
+            driver.FindElement(By.LinkText("Gmail")).Click();
+            driver.Close();
+        }
+    }
+}
